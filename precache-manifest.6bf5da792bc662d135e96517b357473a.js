3f78da2bb1e6a9ab77e5d614ede670ad0cfe60e6
AhmetEnesKCC <ahmetkcc2001@gmail.com>
2020-09-24 08:30:13 +0300

    Updates

---
+++ precache-manifest.6bf5da792bc662d135e96517b357473a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7be4b72fbcf3bed5d537fa2edc1d3e4",
+    "url": "/reacttestapp/index.html"
+  },
+  {
+    "revision": "3ea8fe5e94dcc6ec684e",
+    "url": "/reacttestapp/static/css/main.dcbc11df.chunk.css"
+  },
+  {
+    "revision": "3a377dd21c438065613e",
+    "url": "/reacttestapp/static/js/2.3317037c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/reacttestapp/static/js/2.3317037c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ea8fe5e94dcc6ec684e",
+    "url": "/reacttestapp/static/js/main.c93d8082.chunk.js"
+  },
+  {
+    "revision": "654a0e1e627ff5d27f73",
+    "url": "/reacttestapp/static/js/runtime-main.9c87bce7.js"
+  },
+  {
+    "revision": "4fa4ea43222779743fc8cd4b4835d04e",
+    "url": "/reacttestapp/static/media/NanumGothic-Bold.4fa4ea43.ttf"
+  },
+  {
+    "revision": "159cb67fc3bc762a8c3232f0a0c6728e",
+    "url": "/reacttestapp/static/media/VarelaRound-Regular.159cb67f.ttf"
+  },
+  {
+    "revision": "8f27f5c5ffa2b343c91cd79dc0538006",
+    "url": "/reacttestapp/static/media/logo.8f27f5c5.jpg"
+  }
+]);
